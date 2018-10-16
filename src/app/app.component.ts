739974130b186b57589c279e6264031a3ebd57e6
Carlos Ramos <carlosefrfloresta@gmail.com>
2018-10-16 13:01:53 -0300

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  rows: [string];
+
+  file: FileReader;
+
+  url: string | ArrayBuffer;
+
+  onFileChanged(eventd) {
+    console.log('file: ', eventd)
+    const reader = new FileReader();
+
+    reader.onload = (event: ProgressEvent) => {
+      this.url = (<FileReader>event.target).result;
+    }
+
+    reader.readAsDataURL(eventd.target.files[0])
+    console.log('reader: ', reader)
+    this.file = reader;
+  }
 }
